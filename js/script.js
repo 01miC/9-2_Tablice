b39c5e24871cfaa1e92868d78b5e8c2436f82b0b
Michal Dokupil <michal@dokupil.pl>
2017-12-27 22:03:01 +0100

    Add init-project and script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-var names = ['Asia', 'Kasia', 'Ola', 'Sylwia', 'Ola'];
-var indexOfOla = names.indexOf('Ola');
+var femaleNames = ['Asia', 'Kasia', 'Monika', 'Sylwia', 'Jadwiga', 'Ania'];
+var maleNames = [ 'Tomek', 'Wojtek', 'Jakub', 'Kacper', 'Marcin' ];
+var allNames = femaleNames.concat(maleNames);
+var newName = 'Marian';
 
-console.log('The search value is on ' + indexOfOla + ' position');
-
-console.log(names[indexOfOla]);
+console.log(allNames);
+if (allNames.indexOf(newName) === -1 ) {
+    allNames.push(newName);
+    console.log(allNames);
+}else {
+    console.log('Zle dane');
+}
