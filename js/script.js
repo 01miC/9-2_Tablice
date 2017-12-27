82025c3ab86d2438a20077334f8b424bbd76ea5d
Michal Dokupil <michal@dokupil.pl>
2017-12-27 21:37:44 +0100

    Blank files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var names = ['Asia', 'Kasia', 'Ola', 'Sylwia', 'Ola'];
+var indexOfOla = names.indexOf('Ola');
+
+console.log('The search value is on ' + indexOfOla + ' position');
+
+console.log(names[indexOfOla]);
